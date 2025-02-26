6580001d74938743ace7d4531a5a199e4bbbe4c9
Brayan Ladino <brayladino11@gmail.com>
2025-02-26 14:41:58 -0500

    Tipos de variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+entero = 100
+print   (type(entero))
+decimal = 3.14
+print   (type(decimal))
+letra = "a"
+print   (type(letra))
+mensaje = "Hola"
+print (type(mensaje))
+estado =True
+print (type(estado))
+parrafo = """
+Docente: Jesus David Gacia Caro
+Nombre: Brayan David Ladino
+Proyecto: Ingenieria en sistemas
+"""
+print (type(parrafo))
