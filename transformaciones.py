aafb2b46f74e06a1323a6e7714aa07833d651d25
Brayan Ladino <brayladino11@gmail.com>
2025-02-26 14:59:32 -0500

    Transformacion de variables y solicitudes

---
+++ transformaciones.py
@@ -0,0 +1,21 @@
+
+#Nombre
+nombre = input("Ingrese su nombre")
+
+print (nombre)
+print(type( nombre))
+
+#Edad
+edad = int(input("Ingre edad"))
+print(edad)
+print(type(edad))
+
+#Estado de matricula
+estado = bool(input("estado de matricula: True/False"))
+print (estado)
+print (type(estado))
+
+#Peso
+peso = float(input("eingrese peso: "))
+print(peso)
+print(type(peso))
