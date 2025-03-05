b2ddc9073027b6cf582ca0a56fb21452a5320c64
Brayan Ladino <brayladino11@gmail.com>
2025-03-05 16:17:51 -0500

    Secion

---
+++ inision_secion.py
@@ -0,0 +1,13 @@
+brayan = "brada"
+password = "1234"
+print(type(password))
+
+usuario = input("Ingrese su usario ")
+contraseña = int(input("Inregrese la contraseña "))
+print(type(contraseña))
+
+print(usuario==brayan and password == contraseña)
+
+
+
+
