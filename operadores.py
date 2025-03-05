e05629283b9b1be7ba6367e7309d1b64e3bfc4e3
Brayan Ladino <brayladino11@gmail.com>
2025-03-05 11:25:38 -0500

    Un nuevo problema

---
+++ operadores.py
@@ -0,0 +1,8 @@
+#libreria 
+import math
+ejemplo1 = 10 + (2*(-3*((7-2)+(7*(3-2)))))
+print(ejemplo1)
+ejemplo2 = 2*(4*(7+4*(5*3-9))-3*(40-8))
+print(ejemplo2)
+ejemplo3 = 2*(4*(7+4*(5*3-9))-3*(40-8))
+print(ejemplo3)
