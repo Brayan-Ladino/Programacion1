cbc607030ab13a2b06ca9b3550dfff1dcf59cda7
Brayan Ladino <brayladino11@gmail.com>
2025-03-05 15:32:03 -0500

    Operadores de comparacion

---
+++ operadodes_aritmeticos.py
@@ -0,0 +1,12 @@
+num1 = 5
+num2 = 2
+
+print(num1==num2) #Iguales
+print(num1!=num2) #Diferente
+print(num1>num2)  #mayor
+print(num1<num2)  #menor
+print(num1>=num2) #Mayor igual 
+print(num1<=num2)  #Menor igual
+
+
+
