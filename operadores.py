eab2aeb0889c3a42517892c8fa6fe83a976dfe7e
Brayan Ladino <brayladino11@gmail.com>
2025-03-05 14:40:12 -0500

    Ejemplos de operadores

--- operadores.py
+++ operadores.py
@@ -4,5 +4,9 @@ ejemplo1 = 10 + (2*(-3*((7-2)+(7*(3-2)))))
 print(ejemplo1)
 ejemplo2 = 2*(4*(7+4*(5*3-9))-3*(40-8))
 print(ejemplo2)
-ejemplo3 = 2*(4*(7+4*(5*3-9))-3*(40-8))
+ejemplo3 = 3*(4+(2*(3+5)))+2
 print(ejemplo3)
+ejemplo4 = 2*(4*(7+4*6)-3*32)
+print(ejemplo4)
+
+print(f"Ejemplo 4 = {ejemplo4}")
