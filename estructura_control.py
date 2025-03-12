5eed85293f73d1d6590d7f05f860771d80614b98
Brayan Ladino <brayladino11@gmail.com>
2025-03-12 14:32:40 -0500

    Estrcuturas de control

---
+++ estructura_control.py
@@ -0,0 +1,11 @@
+numero = 19
+
+if numero >5:
+    print("El numero es mayor a 5")
+print("Fuera de la estecutura de control")
+
+#if -else
+if numero >5:
+    print(f"El numero es: {numero}")
+else:
+    print("El numero es menor a 5")
